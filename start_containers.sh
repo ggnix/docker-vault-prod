c4613a1a1e536294acdd378d74b21c913383386e
Gleb <ggrayban@hotmail.com>
2017-05-29 15:01:32 +0300

    Update start_containers.sh

--- start_containers.sh
+++ start_containers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo /usr/local/bin/docker-compose up -d
-sudo docker exec -it docker_vault_container_1 /bin/sh /vault/unlock.sh
+docker-compose up -d
+docker exec -it docker_vault_container_1 /bin/sh /vault/unlock.sh
